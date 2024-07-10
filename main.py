163c5f2bdef1d5838e8227b80ee4a09078a22001
LuisL <dev.eng.luisluna@gmail.com>
2024-07-10 22:21:55 +0000

    siguiendo la clase del video: https://www.loom.com/share/021bc8d61e91444d9efc4f3bd5cc6120

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/", methods = ['GET','POST'])
 def home():
     if(request.method=='GET'):
         return render_template("index.html")
-    else:
+    elif(request.method=='POST'):
         return jsonify({
             "mensaje": "Este es un mensaje dentro de un json"
         })
+    else:
+        return "deleted"
+
+@app.route("/ping", methods=['POST'])
+def pingpong():
+    body = request.get_json()
+    response = {
+        "mesange": body['nombre'] + "he decidido jugar ping pong"        
+    }
+
+@app.route("/saludar/<nombre>", methods=['GET'])
+def saludar(nombre):
+    return "Hola! "+ nombre
+
 
 app.run(host= '0.0.0.0')
